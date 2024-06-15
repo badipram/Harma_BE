61c6013b0bd473e2396b69c7d845d56efc17fb0e
Reza Pradana <159880442+rezavradana@users.noreply.github.com>
2024-06-15 11:35:25 +0700

    Update server.js
    change port and host server

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const { validate } = require('./utils/validate');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
