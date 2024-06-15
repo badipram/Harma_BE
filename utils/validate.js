2b4d85277e2ea42a9b0d5d02001c0e7c32e2fd41
rezavradana <rezapradana472@gmail.comecho>
2024-06-15 20:07:34 +0700

    change commit

--- utils/validate.js
+++ utils/validate.js
@@ -5,8 +5,8 @@ const { User } = require('../models/user');
 
 const validate = async ({ decoded }, request, h) => {
   try {
-    console.log(`Decode: ${decoded}`);
     const { email } = decoded.payload;
+    console.log(`Decode: ${decoded.payload.email}`);
     const user = await User.findOne({
       where: { email: email },
     });
